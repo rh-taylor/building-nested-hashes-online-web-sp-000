4b93f421d5ec38641bb55898e2ab4ab85c8da3e3
Robyn Taylor <rhtaylor0715@gmail.com>
2021-04-27 00:19:47 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,16 +5,11 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        :friend_one => {
-          :name => "Benvolio",
-          :age => "17",
-          :attitude => "worried"
-        }
-        :friend_two => {
-          :name => "Mercutio",
-          :age => "18",
-          :attitude => "hot-headed"
-        }
+      
+      
+      
+      
+      
       ]
    }, 
    :capulet => {
